8c3c4525ae3e0e07db645778437916bba7ef8571
Rohan Rajendra Thite <122561429+ronythite007@users.noreply.github.com>
2026-04-11 19:57:56 +0530

    fix 04

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,11 @@
 import path from "path";
 
 /** @type {import('next').NextConfig} */
+const isVercel = process.env.VERCEL === "1";
+
 const nextConfig = {
   reactStrictMode: true,
-  outputFileTracingRoot: path.join(process.cwd(), ".."),
+  ...(isVercel ? {} : { outputFileTracingRoot: path.join(process.cwd(), "..") }),
 };
 
 export default nextConfig;
